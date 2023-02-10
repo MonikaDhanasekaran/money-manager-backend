25e73b12cc84a359dc9c0a4f11a355a74ef6ec6e
Monika Dhanasekaran <107670734+MonikaDhanasekaran@users.noreply.github.com>
2023-02-10 13:10:11 +0530

    Delete registerRouter.js

--- registerRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const registerModule = require('./modules/registerModule');
-
-router.post('/signup', registerModule.signup);
-
-router.post('/signin', registerModule.signin);
-
-module.exports = router;
